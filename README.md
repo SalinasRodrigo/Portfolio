3bbbed7842ba9d4c2ce5681d52de9ccfdad2e540
Rodrigo Salinas <salinas.e.rodrigo@gmail.com>
2023-09-05 18:43:34 -0400

    Traslado de la información de mi cv a la pagina

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Portfolio Rodrigo Salinas 
+
+## Objetivos:
+
+  - [x] Trasladar la información de mi cv
+    - [x] Speech personal.
+    - [x] Habilidades.
+    - [x] Proyectos.
+    - [X] Educación.
+  
--- proyecto-portfolio/src/App.jsx
+++ proyecto-portfolio/src/App.jsx
@@ -1,7 +1,66 @@
 import "./App.css";
+import {cv} from "./mooks/cv.json"
 
 function App() {
-  return "que onda que pex";
+  return (
+    <>
+      <h1>Rodrigo Salinas</h1>
+      <p>{cv.personaleSpeech}</p>
+
+      <h2>Habilidades</h2>
+      
+      <div className="habilidades">
+        <div className="habilidad">
+          <h3>Frontend</h3>
+          <ul>
+            {cv.habilidadesTecnicas.frontent.map((habilidad, index)=>{
+              return (
+                <li key={index}>{habilidad}</li>
+              )
+            })}
+          </ul>
+        </div>
+
+        <div className="habilidad">
+          <h3>Backend</h3>
+          <ul>
+            {cv.habilidadesTecnicas.backend.map((habilidad, index)=>{
+              return (
+                <li key={index}>{habilidad}</li>
+              )
+            })}
+          </ul>
+        </div>
+
+      </div>
+
+      <h2>Proyectos</h2>
+      <ul style={{  
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center"
+      }}>
+      {cv.proyectos.map((proyecto, index) => {
+        return (
+          <li key={index}>{proyecto}</li>
+        )
+      })}
+      </ul>
+
+      <h2>Educación</h2>
+      <ul style={{  
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center"
+      }}>
+      {cv.educación.map((escuela, index) => {
+        return (
+          <li key={index}>{escuela}</li>
+        )
+      })}
+      </ul>
+    </>
+    );
 }
 
 export default App;
